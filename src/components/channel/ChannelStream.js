005a73507f6774ce9e63964e7f97596f3513a596
Fryzu <bartek.fryz@gmail.com>
2019-12-30 23:05:26 +0100

    Adds video component to the channel

--- src/components/channel/ChannelStream.js
+++ src/components/channel/ChannelStream.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
-import { Card } from 'react-bootstrap';
 
 export default class ChannelStream extends Component {
   render() {
     return (
-      <div>
-        <Card.Body>Hello!</Card.Body>
+      <div className="flexChild p-3" id="camera-container">
+        <video width="100%" autoplay controls>
+          <source src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4" />
+        </video>
       </div>
     );
   }
 }
+
+// style={{
+//   position: 'absolute',
+//   left: 0,
+//   top: 0,
+//   height: '100%',
+//   width: '100%',
+// }}
